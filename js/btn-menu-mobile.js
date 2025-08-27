66c4b0db93be150e9d480a7ffe8a6b0ccfa13b43
SamuelFortes <samuel.fortes@ufpi.edu.br>
2025-08-27 00:51:17 -0300

    finalização do projeto

--- js/btn-menu-mobile.js
+++ js/btn-menu-mobile.js
@@ -1,8 +1,12 @@
 let btnMenu = document.getElementById("btn__menu__mobile");
 let line1 = document.querySelector(".line1__btn__mobile");
 let line2 = document.querySelector(".line2__btn__mobile");
+let menuBtn = document.getElementById("header__menu__mobile");
+let body = document.body;
 
 btnMenu.addEventListener("click", () => {
     line1.classList.toggle("activate__line1");
     line2.classList.toggle("activate__line2");
+    menuBtn.classList.toggle("open");
+    body.classList.toggle("no__overflow");
 })
